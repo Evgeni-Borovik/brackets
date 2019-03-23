5e1f32e2690ee71b182d495770098a50a13a571e
Evgeni-Borovik <anyuser1980@gmail.com>
2019-03-24 00:55:05 +0300

    feature: add check for similar open and close brackets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  var output = true;
+  let output = true;
 
   // Step 1: check if quantity of brackets is even; if not - the output is "false"
   if (str.length % 2 !== 0) {
@@ -10,7 +9,7 @@ module.exports = function check(str, bracketsConfig) {
     // Step 2: check if the first bracket is closing; if yes - the output is "false"
   } else {
     for (let k in bracketsConfig) {
-      if (bracketsConfig[k][1] === str[0]) {
+      if (bracketsConfig[k][0] !== bracketsConfig[k][1] && bracketsConfig[k][1] === str[0]) {
         output = false;
         break;
       }
@@ -19,10 +18,14 @@ module.exports = function check(str, bracketsConfig) {
 
   // Step 3: (if 1 and 2 passed) check array of brackets
   if (output) {
-    var storage = [];
+    let storage = [];
     for (let i in str) {
       for (let k in bracketsConfig) {
         if (str[i] === bracketsConfig[k][0]) {
+          if (bracketsConfig[k][0] === bracketsConfig[k][1] && str[i] === storage[storage.length - 1]) {
+            storage.pop();
+            break;
+          }
           storage.push(str[i]);
           break;
         }
